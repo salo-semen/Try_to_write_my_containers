611c9fb159bcde39509ba3f48a03f2f2c7799742
Семён Салофутдинов <semensalofutdinov@MacBook-Air-Semen.local>
2026-09-25 22:01:01 +0700

    add plans for update vector

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,36 +1,10 @@
 #include "vector.h"
-
-template <typename T>
-Vector<T>::Vector() {
-    Array_ = new T[Capacity_];
-}
-
-template <typename T>
-Vector<T>::~Vector() {
-    delete[] Array_;
-}
-
-template <typename T>
-void Vector<T>::push_back(T x) {
-    if (Size_ < Capacity_) {
-        Array_[Size_] = x;
-    } else {
-        T* newArray = new T[Capacity_ * 2];
-        for (size_t i = 0; i != Capacity_; ++i) {
-            newArray[i] = Array_[i];
-        }
-        T* temp = Array_;
-        Array_ = newArray;
-        newArray = temp;
-        delete[] newArray;
-
-        Capacity_ *= 2;
-    }
-    ++Size_;
-}
+#include <iostream>
 
 int main() {
     Vector<int> v;
+    std::cout << v.empty() << std::endl;
     v.push_back(1);
     v.push_back(2);
+
 }
--- vector/vector.h
+++ vector/vector.h
@@ -6,9 +6,71 @@ public:
     Vector();
     Vector(const Vector& other);
     ~Vector();
-    void push_back(T x);
+    void push_back(const T& x);
+    size_t size();
+    size_t capacity();
+    bool empty();
+
+    void reserve(int n);
+    void resize(int n);
+    T operator[](size_t index);
+    T at(size_t index);
+    T front();
+    T back();
+    T* data();
+    void push_back(T&& x);
+    void pop_back();
+    void clear();
+    void insert();
+    void erase();
 private:
         T* Array_ = nullptr;
         size_t Size_ = 0;
         size_t Capacity_ = 1;
 };
+
+//Добавить Итерататоры, выделение памяти через аллокаторы и умные указатели, конструкторы копирования, перемещения
+
+template <typename T>
+Vector<T>::Vector() {
+    Array_ = new T[Capacity_];
+}
+
+template <typename T>
+Vector<T>::~Vector() {
+    delete[] Array_;
+}
+
+template <typename T>
+void Vector<T>::push_back(const T& x) {
+    if (Size_ < Capacity_) {
+        Array_[Size_] = x;
+    } else {
+        T* newArray = new T[Capacity_ * 2];
+        for (size_t i = 0; i != Capacity_; ++i) {
+            newArray[i] = Array_[i];
+        }
+        T* temp = Array_;
+        Array_ = newArray;
+        newArray = temp;
+        delete[] newArray;
+
+        Capacity_ *= 2;
+    }
+    ++Size_;
+}
+
+template <typename T>
+size_t Vector<T>::size() {
+    return Size_;
+}
+
+template <typename T>
+size_t Vector<T>::capacity() {
+    return Capacity_;
+}
+
+template <typename T>
+bool Vector<T>::empty() {
+    return size();
+}
